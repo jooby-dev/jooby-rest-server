71a675d6d2153ee76373b177312237586a1e4f4c
p.poukh <p.poukh@infomir.com>
2024-03-04 18:03:18 +0200

    controllers. implement mtxLora encoder

--- src/controllers/encoders/mtxLora.js
+++ src/controllers/encoders/mtxLora.js
@@ -1,15 +1,36 @@
-import errors from '../../errors.js';
-import {analog, utils} from 'jooby-codec/index.js';
-import * as Frame from 'jooby-codec/utils/frame.js';
-import {requestById, responseById} from 'jooby-codec/analog/constants/commandRelations.js';
+import {analog, mtxLora, utils} from 'jooby-codec/index.js';
+import {splitBytesToDataSegments} from 'jooby-codec/analog/splitBytesToDataSegments.js';
+import {requestById as mtxRequestById, responseById as mtxResponseById} from 'jooby-codec/mtx/constants/commandRelations.js';
+import {requestById as mtxLoraRequestById, responseById as mtxLoraResponseById} from 'jooby-codec/mtxLora/constants/commandRelations.js';
+import * as directions from 'jooby-codec/constants/directions.js';
+import {accessLevels} from 'jooby-codec/mtx/constants/index.js';
 import {HDLC} from '../../constants/framingFormats.js';
+import errors from '../../errors.js';
 
 
-const constructCommand = command => {
-    const constructor = requestById.get(command.id) || responseById.get(command.id);
+const constructDownlinkMtxLoraCommands = commands => commands.map(command => {
+    const constructor = mtxLoraRequestById.get(command.id);
 
-    return new constructor(command);
-};
+    return constructor ? new constructor(command) : null;
+});
+
+const constructUplinkMtxLoraCommands = commands => commands.map(command => {
+    const constructor = mtxLoraResponseById.get(command.id);
+
+    return constructor ? new constructor(command) : null;
+});
+
+const constructDownlinkMtxCommands = commands => commands.map(command => {
+    const constructor = mtxRequestById.get(command.id);
+
+    return constructor ? new constructor(command) : null;
+});
+
+const constructUplinkMtxCommands = commands => commands.map(command => {
+    const constructor = mtxResponseById.get(command.id);
+
+    return constructor ? new constructor(command) : null;
+});
 
 
 /**
@@ -18,24 +39,44 @@ const constructCommand = command => {
 export default function encode ( {body}, reply ) {
     try {
         const {
-            framingFormat,
-            frame,
+            commands,
             bytesConversionFormat,
+            direction,
+            framingFormat,
+            messageId,
             response
         } = body;
 
-        const {commands} = framingFormat === HDLC ? frame : body;
-
-        let bytes = analog.message.toBytes(commands.map(constructCommand));
-
         if ( framingFormat === HDLC ) {
-            bytes = Frame.toFrame(bytes).bytes;
-            response.frame.data = utils.getStringFromBytes(bytes, bytesConversionFormat);
-        } else {
-            response.data = utils.getStringFromBytes(bytes, bytesConversionFormat);
+            reply.sendError(errors.BAD_REQUEST, 'The HDLC framing format is not supported for the mtxLora');
+
+            return;
         }
 
-        reply.send(response);
+        let mtxCommands = direction === directions.UPLINK
+            ? constructUplinkMtxLoraCommands(commands)
+            : constructDownlinkMtxLoraCommands(commands);
+
+        let accessLevel = accessLevels.UNENCRYPTED;
+
+        if ( mtxCommands.some(command => command === null) ) {
+            mtxCommands = direction === directions.UPLINK
+                ? constructUplinkMtxCommands(commands)
+                : constructDownlinkMtxCommands(commands);
+            accessLevel = body.accessLevel;
+        }
+
+        const mtxBytes = mtxLora.message.toBytes(messageId, mtxCommands, {accessLevel, ...body});
+        const dataSegmentCommands = splitBytesToDataSegments(mtxBytes, {maxSegmentSize: 50, ...body});
+
+        const segmentBytes = dataSegmentCommands.map(
+            dataSegmentCommand => (utils.getStringFromBytes(analog.message.toBytes([dataSegmentCommand]), bytesConversionFormat))
+        );
+
+        reply.send({
+            ...response,
+            segments: segmentBytes
+        });
     } catch ( error ) {
         reply.sendError(errors.BAD_REQUEST, error);
     }
