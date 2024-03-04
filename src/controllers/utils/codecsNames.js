0cdbcde51bb2574e65bfc31e3bc8d828beb2877e
p.poukh <p.poukh@infomir.com>
2024-03-04 18:07:30 +0200

    remove obsolete files

--- src/controllers/utils/codecsNames.js
+++
@@ -1,9 +0,0 @@
-import {analog, obisObserver, mtx, mtxLora} from 'jooby-codec/index.js';
-
-
-export default new Map([
-    [analog, 'analog'],
-    [obisObserver, 'obisObserver'],
-    [mtx, 'mtx'],
-    [mtxLora, 'mtxLora']
-]);
