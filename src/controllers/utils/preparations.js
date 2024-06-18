03753e7a186efb6003472a2d4ec9b2829309390a
p.poukh <p.poukh@infomir.com>
2024-06-18 17:15:48 +0300

    controllers/utils: use jooby-codec v4 version in preparation functions

--- src/controllers/utils/preparations.js
+++ src/controllers/utils/preparations.js
@@ -1,21 +1,33 @@
 import getStringFromBytes from '../../utils/getStringFromBytes.js';
 
-const prepareCommand = ( command, options ) => {
-    const {constructor: {id, name}} = command;
-    let result = {id, name};
 
-    try {
-        result = {...result, ...JSON.parse(command.toJson(options))};
-        // eslint-disable-next-line no-empty
-    } catch {}
+const prepareCommand = ( commandsById, command, options ) => {
+    const {id, name} = command;
+    const commandNamespace = commandsById[command.id];
 
-    return result;
+    const parameters = commandNamespace && 'toJson' in commandNamespace && typeof commandNamespace.toJson === 'function'
+        ? JSON.parse(commandNamespace.toJson(command.parameters, options))
+        : command.parameters;
+
+    return parameters && Object.keys(parameters).length !== 0 ? {id, name, parameters} : {id, name};
 };
 
-export const prepareCommands = ( commands, options ) => commands.map(({command}) => prepareCommand(command, options));
+const prepareCommandWithError = (commandsById, {error, command}, options ) => {
+    const preparedCommand = prepareCommand(commandsById, command, options);
 
-export const prepareFrame = ( {isValid, bytes, content}, options ) => ({
-    isValid,
+    return command.bytes
+        ? {...preparedCommand, error, data: getStringFromBytes(command.bytes, options)}
+        : {...preparedCommand, error};
+};
+
+export const prepareCommands = ( commandsById, commands, options ) => (
+    commands.map(command => (command.error
+        ? prepareCommandWithError(commandsById, command, options)
+        : prepareCommand(commandsById, command, options)))
+);
+
+export const prepareFrame = ( {header, bytes, payload}, options ) => ({
+    ...header,
     bytes: getStringFromBytes(bytes, options),
-    content: getStringFromBytes(content, options)
+    payload: getStringFromBytes(payload, options)
 });
