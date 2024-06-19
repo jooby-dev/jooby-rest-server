d7c7cea895c955cb591bddac9abe83f246e4e92e
p.poukh <p.poukh@infomir.com>
2024-06-19 18:11:40 +0300

    utills: review results

--- src/utils/getBytesFromString.js
+++ src/utils/getBytesFromString.js
@@ -2,9 +2,9 @@ import {getBytesFromHex, getBytesFromBase64} from '@jooby-dev/jooby-codec/utils/
 import {HEX} from '@jooby-dev/jooby-codec/constants/bytesConversionFormats.js';
 
 
-const getBytesFromString = ( bytes, bytesConversionFormats = HEX ) => (bytesConversionFormats === HEX
-    ? getBytesFromHex(bytes)
-    : getBytesFromBase64(bytes)
+const getBytesFromString = ( bytes, bytesConversionFormats = HEX ) => (
+    bytesConversionFormats === HEX
+        ? getBytesFromHex(bytes) : getBytesFromBase64(bytes)
 );
 
 export default getBytesFromString;
