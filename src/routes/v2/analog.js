4db84b402f8cd9143b702017950b302af9cde91e
p.poukh <p.poukh@infomir.com>
2024-07-05 03:14:06 +0300

    routes: add adapter to analog, mtx and obisObserver routes

--- src/routes/v2/analog.js
+++ src/routes/v2/analog.js
@@ -2,6 +2,7 @@ import {validateDecoder, validateEncoder} from './validators/analog.js';
 import {modifyDecoderRequest, modifyEncoderRequest} from './utils/modifyRequest.js';
 import decode from '../../controllers/decoders/analog.js';
 import encode from '../../controllers/encoders/analog.js';
+import adaptData from '../../adapters/index.js';
 import {ANALOG} from '../../constants/protocols.js';
 
 
@@ -9,7 +10,7 @@ export default fastify => {
     fastify.post(
         `/decoder/${ANALOG}`,
         {
-            preValidation: [validateDecoder],
+            preValidation: [adaptData, validateDecoder],
             preHandler: [modifyDecoderRequest]
         },
         decode
--- src/routes/v2/obisObserver.js
+++ src/routes/v2/obisObserver.js
@@ -2,6 +2,7 @@ import {validateDecoder, validateEncoder} from './validators/obisObserver.js';
 import {modifyDecoderRequest, modifyEncoderRequest} from './utils/modifyRequest.js';
 import decode from '../../controllers/decoders/obisObserver.js';
 import encode from '../../controllers/encoders/obisObserver.js';
+import adaptData from '../../adapters/index.js';
 import {OBIS_OBSERVER} from '../../constants/protocols.js';
 
 
@@ -9,7 +10,7 @@ export default fastify => {
     fastify.post(
         `/decoder/${OBIS_OBSERVER}`,
         {
-            preValidation: [validateDecoder],
+            preValidation: [adaptData, validateDecoder],
             preHandler: [modifyDecoderRequest]
         },
         decode
